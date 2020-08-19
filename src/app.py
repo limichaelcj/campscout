3bab1a708c9eef6cf46b9ba7c1bb8d150d2c8e91
Michael Li <mcli.893@gmail.com>
2020-08-19 00:10:31 -0700

    add facilities api call feature

--- src/app.py
+++ src/app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+import os
+import requests
+from flask import Flask, request, render_template
 app = Flask(__name__)
 app.config.from_pyfile('../settings.py')
+
+api_root = 'https://ridb.recreation.gov/api/v1'
 
 @app.route('/')
 def index():
@@ -8,5 +12,18 @@ def index():
 
 @app.route('/facilities')
 def facilities():
+
+    query = request.args.get('query')
+
+    api_endpoint = f'{api_root}/facilities?query={query}&limit=50&offset=0&state=CA&activity=CAMPING&lastupdated=10-01-2018'
+    headers = {
+        'accept': 'application/json',
+        'apiKey': os.getenv('RIDB_API_KEY')
+    }
+
+    response = requests.get(api_endpoint, headers=headers)
+    json = response.json()
+    facilities = json['RECDATA']
+
     # request facilities data from ridb
-    return render_template('facilities.html')
+    return render_template('facilities.html', facilities=facilities)
